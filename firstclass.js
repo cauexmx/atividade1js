d83d388c3efb9c660795fdc1ec5adbb343ff7393
cauexmx <127966350+cauexmx@users.noreply.github.com>
2023-07-26 13:42:55 -0300

    Add files via upload

---
+++ firstclass.js
@@ -0,0 +1,71 @@
+/* 1 */
+console.log('Eu tenho 16 anos')
+
+/* 2 */
+console.log('Programei por 4 semanas ao todo nas férias')
+
+/* 3
+
+console.log('Usando o comentário')
+Começa na barra e ateristico e inverte
+no final
+*/
+
+/* 4 */
+let num = 16;
+
+let girlfriend = false
+
+const user = {
+    name: 'Cauê',
+    age: 16, 
+    isActive: false,
+};
+
+console.log(user)
+
+let message = 'Estou muito contente';
+
+checkpoint = null; 
+
+/* 5 */
+idadec = 16
+console.log( idadec + 3.5)
+
+/* 6 */
+Atual = new Date (2023)
+Lua = new Date(1969)
+console.log( Atual - Lua )
+
+/* 7 */
+console.log(65/240)
+
+/* 8 */
+console.log(0.2708*100)
+
+/* 9 */
+console.log('Hello' + 'World')
+
+/* 10 */
+console.log('Hello' + ' ' + 'World')
+
+/* 11 */
+console.log('Cauezinho'.length)
+
+/* 12 */
+console.log('kayan novaes'.toUpperCase());
+
+/* 13 */
+console.log(' Professor Kayan Novaes '.trim())
+
+/* 14 */
+console.log(Math.floor((Math.random()*100)))
+
+/* 15 */
+console.log(Math.floor((Math.ceil(43.8))))
+
+/* 16 */
+ponto = Number.isInteger(2017)
+console.log(ponto)
+
+
